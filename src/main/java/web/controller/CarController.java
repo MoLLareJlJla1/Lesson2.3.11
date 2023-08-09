30cbc2ee6f4a95f1c83834771268d598ba12e373
MoLLareJlJla1 <MoLLareJlJla1@mail.ru>
2023-08-09 23:52:02 +0300

    Done!

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,6 +1,7 @@
 package web.controller;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarService;
@@ -8,13 +9,17 @@ import web.Service.CarService;
 @Controller
 public class CarController {
     private CarService carService;
+
     public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping("/cars")
-    public String allcar(@RequestParam(name = "count",defaultValue = "5") int id, ModelMap model) {
-        model.addAttribute("allcars",carService.getCarLimit(id));
+    public String allcar(@RequestParam(name = "count", defaultValue = "5") int count, Model model) {
+        if (count < 0) {
+            throw new IllegalArgumentException("Отрицательное число");
+        }
+        model.addAttribute("allcars", carService.getCarLimit(count));
         return "car";
     }
 
